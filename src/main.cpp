11226ea1128b010ba3860ea4aa7a77179b8270eb
AlttiAir <alttiairaksinen@kamk.fi>
2026-03-28 14:07:09 +0200

    Added some windows input handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,45 @@ std::string getAsset(std::string assetName) {
     return ASSET_DIR + std::string("/" + assetName);
 }
 
+void isKeyPressed(int key, bool& buttonGate) {
+    if (GetAsyncKeyState(key) < 0 && buttonGate == false)
+    {
+        std::cout << "ALT key is down." << std::endl;
+        buttonGate = true;
+    }
+    if (GetAsyncKeyState(key) == 0 && buttonGate == true)
+    {
+        std::cout << "ALT key is RELEASED!" << std::endl;
+        buttonGate = false;
+    }
+}
+
 int main() {
     std::cout << "Test: " << getAsset("Testings") << std::endl;
+
+    bool exitable = false, buttonGate = false;
+
+    while (!exitable) {
+        /*if (isKeyPressed(VK_SHIFT, gate)) {
+            std::cout << "ALT key is down." << std::endl;
+        }
+        else {
+            std::cout << "ALT key is RELEASED!" << std::endl;
+        }*/
+
+        isKeyPressed(VK_SHIFT, buttonGate);
+
+        /*if (GetAsyncKeyState(VK_SHIFT) < 0 && gate == false)
+        {
+            std::cout << "ALT key is down." << std::endl;
+            gate = true;
+        }
+        if (GetAsyncKeyState(VK_SHIFT) == 0 && gate == true)
+        {
+            std::cout << "ALT key is RELEASED!" << std::endl;
+            gate = false;
+        }*/
+
+    }
     return 0;
 }
