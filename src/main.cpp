5495f8a6a67eef917ce2acd81224257418bc3edc
AlttiAir <alttiairaksinen@kamk.fi>
2026-04-06 02:58:22 +0300

    sine wave audio callback example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <windows.h>
 
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
+
+#define DEVICE_FORMAT       ma_format_f32
+#define DEVICE_CHANNELS     2
+#define DEVICE_SAMPLE_RATE  48000
+
+bool simpleGate = false;
 
 std::string getAsset(std::string assetName) {
     return ASSET_DIR + std::string("/" + assetName);
@@ -19,6 +27,7 @@ void isKeyPressed(int key, bool& buttonGate) {
         std::cout << key << " key is RELEASED!" << std::endl;
         buttonGate = false;
     }
+    simpleGate = buttonGate;
 }
 
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
@@ -26,6 +35,18 @@ void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uin
     // In playback mode copy data to pOutput. In capture mode read data from pInput. In full-duplex mode, both
     // pOutput and pInput will be valid and you can move data from pInput into pOutput. Never process more than
     // frameCount frames.
+    if (simpleGate) {
+        ma_waveform* pSineWave;
+
+        MA_ASSERT(pDevice->playback.channels == DEVICE_CHANNELS);
+
+        pSineWave = (ma_waveform*)pDevice->pUserData;
+        MA_ASSERT(pSineWave != NULL);
+
+        ma_waveform_read_pcm_frames(pSineWave, pOutput, frameCount, NULL);
+
+        (void)pInput;   /* Unused. */
+    }
 }
 
 int main() {
@@ -33,20 +54,33 @@ int main() {
 
     bool exitable = false, buttonGate = false;
 
-    ma_device_config config = ma_device_config_init(ma_device_type_playback);
-    config.playback.format = ma_format_f32;   // Set to ma_format_unknown to use the device's native format.
-    config.playback.channels = 2;               // Set to 0 to use the device's native channel count.
-    config.sampleRate = 48000;           // Set to 0 to use the device's native sample rate.
-    config.dataCallback = data_callback;   // This function will be called when miniaudio needs more data.
-    //config.pUserData = pMyCustomData;   // Can be accessed from the device object (device.pUserData).
-
+    ma_waveform sineWave;
+    ma_device_config deviceConfig;
     ma_device device;
-    if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
-        return -1;  // Failed to initialize the device.
-    }
-    std::cout << "Device successful" << std::endl;
+    ma_waveform_config sineWaveConfig;
 
-    ma_device_start(&device);     // The device is sleeping by default so you'll need to start it manually.
+    deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format = DEVICE_FORMAT;
+    deviceConfig.playback.channels = DEVICE_CHANNELS;
+    deviceConfig.sampleRate = DEVICE_SAMPLE_RATE;
+    deviceConfig.dataCallback = data_callback;
+    deviceConfig.pUserData = &sineWave;
+
+    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+        printf("Failed to open playback device.\n");
+        return -4;
+    }
+
+    printf("Device Name: %s\n", device.playback.name);
+
+    sineWaveConfig = ma_waveform_config_init(device.playback.format, device.playback.channels, device.sampleRate, ma_waveform_type_sine, 0.2, 220);
+    ma_waveform_init(&sineWaveConfig, &sineWave);
+
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        return -5;
+    }
 
     while (!exitable) {
         isKeyPressed(VK_SHIFT, buttonGate);
